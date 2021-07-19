37bd4524f7a1f65d6886790f5093e0d7af1f0a64
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2021-07-19 15:03:05 +0600

    bazar

---
+++ js/class14.js
@@ -0,0 +1,12 @@
+let 
+your_account_balance =500
+product_cost =0
+
+function all_bazar(your_account_balance,product_cost){
+    console.log(your_account_balance,product_cost);
+
+    return (your_account_balance-product_cost)
+}
+
+remaining =all_bazar(your_account_balance,product_cost)
+    console.log(remaining);
